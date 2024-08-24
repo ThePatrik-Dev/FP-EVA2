2330dd5c6137b1f886988e54f42e964ec736b484
Joel Alderete <thej14653@gmail.com>
2024-08-23 23:18:28 -0400

    Add files via upload

---
+++ EVA_2_VARIABLES/src/eva_2_variables/EVA_2_VARIABLES.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_variables;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                            // Tipos de Datos
+        System.out.println("Hola Mundo!!"); // Cadena de Texto
+        System.out.println(100); // Entero
+        System.out.println(3.1416); // Numero con Punto Flotante
+        System.out.println('c'); // Un Caracter
+        System.out.println(true); // Un Valor Logico (Boolaeano)
+        System.out.println(     false    
+        )
+                // Alderete Aguirre Diego Joel
+                ; // ; --> Fin de Introduccion (No Olvidar Ponerlo)
+        
+        // Numeros Enteros
+        // Int
+        // Byte
+        // Short
+        // Long
+
+        // Numeros con Parte Decimal
+        // Float
+        // Double
+        
+        // Cadena de Texto
+        // String
+        // Char
+        
+        // Valores Logicos
+        // Boolean
+        
+        // Declaraciones
+        // Sintaxis para Declarar Variablas (Reglas)
+        // Tipo_De_Dato Nombre_De_La_Variable
+        
+        String nombre;
+        String Apellidoo;
+        String CURP;
+        float Promedio;
+        int Edad;
+        
+    }
+    
+}
