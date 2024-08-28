376d83f2be4c4b4058782be29a3ec49b4d26eff8
Joel Alderete <thej14653@gmail.com>
2024-08-28 17:52:58 -0600

    Versión Completa del EVA 2

---
+++ EVA_2_VARIABLES.2/src/eva_2_variables/pkg2/EVA_2_VARIABLES2.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_variables.pkg2;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA_2_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+                                                    // Tipos de Datos
+        System.out.println("Hola Mundo!!"); // Cadena de Texto
+        System.out.println(100); // Entero
+        System.out.println(3.1416); // Numero con Punto Flotante
+        System.out.println('c'); // Un Caracter
+        System.out.println(true); // Un Valor Logico (Boolaeano)
+        System.out.println(     false    
+        )
+                // Alderete Aguirre Diego Joel
+                ; // ; --> Fin de Introduccion (No Olvidar Ponerlo)
+        
+        String Nombre;
+        String Apellido;
+        String CURP;
+        float Promedio;
+        int Edad;
+        
+        Nombre = "Diego Joel";
+        Apellido = "Alderete Aguirre";
+        Promedio = (float) 9.2;
+        Edad = 18;
+        
+        System.out.println(Nombre);
+        System.out.println(Apellido);
+        System.out.println(Promedio);
+        System.out.println(Edad);
+        
+    }
+        // Numeros Enteros
+        // Int
+        // Byte
+        // Short
+        // Long
+
+        // Numeros con Parte Decimal
+        // Float
+        // Double
+        
+        // Cadena de Texto
+        // String
+        // Char
+        
+        // Valores Logicos
+        // Boolean
+        
+        // Declaraciones
+        // Sintaxis para Declarar Variablas (Reglas)
+        // Tipo_De_Dato Nombre_De_La_Variable
+}
